e17e5eb4a3dd022a86f244ca790f6a4030d89be6
vsvsvsvsvsvs <alaverduyck@gmail.com>
2025-12-09 11:30:10 +0300

    Update Lexer.h (добавлена функция TokenTypeToString для вывода)

--- Lexer.h
+++ Lexer.h
@@ -45,4 +45,53 @@ class Lexer {
                            int start_col) const;
 };
 
+static const char* TokenTypeToString(TokenType t) {
+  switch (t) {
+    case TokenType::KW_FN: return "KW_FN";
+    case TokenType::KW_INT: return "KW_INT";
+    case TokenType::KW_CHAR: return "KW_CHAR";
+    case TokenType::KW_DOUBLE: return "KW_DOUBLE";
+    case TokenType::KW_STRING: return "KW_STRING";
+
+    case TokenType::KW_IF: return "KW_IF";
+    case TokenType::KW_ELSE: return "KW_ELSE";
+    case TokenType::KW_WHILE: return "KW_WHILE";
+    case TokenType::KW_DO: return "KW_DO";
+    case TokenType::KW_FOR: return "KW_FOR";
+    case TokenType::KW_RETURN: return "KW_RETURN";
+    case TokenType::KW_BREAK: return "KW_BREAK";
+    case TokenType::KW_CONTINUE: return "KW_CONTINUE";
+
+    case TokenType::IDENTIFIER: return "IDENTIFIER";
+    case TokenType::NUMBER: return "NUMBER";
+    case TokenType::STRING_LITERAL: return "STRING_LITERAL";
+
+    case TokenType::PLUS: return "+";
+    case TokenType::MINUS: return "-";
+    case TokenType::STAR: return "*";
+    case TokenType::SLASH: return "/";
+    case TokenType::PERCENT: return "%";
+
+    case TokenType::ASSIGN: return "=";
+    case TokenType::EQ: return "==";
+    case TokenType::NEQ: return "!=";
+    case TokenType::LT: return "<";
+    case TokenType::LE: return "<=";
+    case TokenType::GT: return ">";
+    case TokenType::GE: return ">=";
+
+    case TokenType::LPAREN: return "(";
+    case TokenType::RPAREN: return ")";
+    case TokenType::LBRACE: return "{";
+    case TokenType::RBRACE: return "}";
+    case TokenType::LBRACKET: return "[";
+    case TokenType::RBRACKET: return "]";
+    case TokenType::SEMICOLON: return ";";
+    case TokenType::COMMA: return ",";
+
+    case TokenType::END_OF_FILE: return "EOF";
+    default: return "UNKNOWN";
+  }
+}
+
 #endif
