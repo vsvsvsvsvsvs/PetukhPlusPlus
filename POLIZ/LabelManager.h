85d217e349d2fc53b7dea2aa1e40fba5d05babd1
vsvsvsvsvsvs <alaverduyck@gmail.com>
2026-02-12 11:46:25 +0300

    Create LabelManager.h

---
+++ POLIZ/LabelManager.h
@@ -0,0 +1,15 @@
+#ifndef LABEL_MANAGER_H
+#define LABEL_MANAGER_H
+
+#include <string>
+
+class LabelManager {
+  int counter = 0;
+
+public:
+  std::string NewLabel() {
+    return "L" + std::to_string(counter++);
+  }
+};
+
+#endif
